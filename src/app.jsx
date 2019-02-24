7d5811820eddad7fa39903ab3b5bc31437e376d7
Ari Petäjäjärvi <fpvonly@gmail.com>
2019-02-24 15:22:58 +0200

    Config work

---
+++ src/app.jsx
@@ -0,0 +1,6 @@
+import '@babel/polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import MainUI from './components/MainUI.jsx';
+
+ReactDOM.render(<div>WUI</div>, document.getElementById('app'));
