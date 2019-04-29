90e7653de624a76b66e9b15c36242a1ec2c6b664
Ari Petäjäjärvi <fpvonly@gmail.com>
2019-04-29 21:37:57 +0300

    Main menu area closing functionality

--- src/components/MainUI.jsx
+++ src/components/MainUI.jsx
@@ -12,6 +12,8 @@ class UI extends React.Component {
   constructor(props) {
     super(props);
 
+    this.ui = false;
+
     this.state = {
       isHydrating: true,
       menuOpen: false
@@ -19,12 +21,22 @@ class UI extends React.Component {
   }
 
   componentDidMount() {
-
+    this.ui.addEventListener('click', this.closeMenu, false);
   }
 
+  componentWillUnMount() {
+    this.ui.removeEventListener('click', this.closeMenu, false);
+  }
 
   handleStartClick = () => {
     this.setState({menuOpen: !this.state.menuOpen});
+  }
+
+  closeMenu = (e) => {
+    if (e.target.className === 'ui') {
+      e.stopPropagation();
+      this.setState({menuOpen: false});
+    }
   }
 
   render() {
@@ -33,7 +45,7 @@ class UI extends React.Component {
     taskbar = <TaskBar menuOpen={this.state.menuOpen} handleStartClick={this.handleStartClick} />;
     menu = (this.state.menuOpen === true) ? <MenuArea /> : null;
 
-    return <div className='ui'>
+    return <div ref={(c) => { this.ui = c; }} className='ui'>
       {menu}
       {taskbar}
     </div>;
