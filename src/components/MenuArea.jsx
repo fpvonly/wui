1e51b3fe1976959db4cbf3aab1ac4fff60fd56ac
Ari Petäjäjärvi <fpvonly@gmail.com>
2019-05-08 19:31:53 +0300

    Working on resizable menu area

--- src/components/MenuArea.jsx
+++ src/components/MenuArea.jsx
@@ -8,6 +8,10 @@ class MenuArea extends React.Component {
   constructor(props) {
     super(props);
 
+    this.resizeGrip = null;
+    this.panel = null;
+    this.menu = null;
+    this.startX = null;
   }
 
   static defaultProps = {
@@ -19,12 +23,30 @@ class MenuArea extends React.Component {
   };
 
   componentDidMount() {
+    this.resizeGrip.addEventListener('mousedown', this.handleMouseDown, false);
+  }
 
+  handleMouseDown = (e) => {
+    this.startX = e.pageX;
+    this.startWidth = this.menu.getBoundingClientRect().width;
+    window.addEventListener('mousemove', this.handleMouseMove, false);
+    window.addEventListener('mouseup', this.handleMouseUp, false);
+  }
+
+  handleMouseMove = (e) => {
+    let diffX = (this.startX < e.pageX) ? this.startX - e.pageX : this.startX - e.pageX;
+    let newWidth = (this.startWidth) - diffX;
+
+    this.menu.style.width = newWidth + 'px';
+  }
+
+  handleMouseUp = (e) => {
+    window.removeEventListener('mousemove', this.handleMouseMove, false);
   }
 
 
   render() {
-    return <div className='menu'>
+    return <div className='menu' ref={(c) => { this.menu = c; }}>
       <div className='side_panel'>
         <div className='btn'>
         Btn
@@ -36,7 +58,7 @@ class MenuArea extends React.Component {
         Btn
         </div>
       </div>
-      <div className='content_panel'>
+      <div className='content_panel' ref={(c) => { this.panel = c; }}>
         <div className='box'></div>
         <div className='box'></div>
         <div className='box'></div>
@@ -44,7 +66,7 @@ class MenuArea extends React.Component {
         <div className='box'></div>
 
       </div>
-      <div className='resize_grip' />
+      <div className='resize_grip' ref={(c) => { this.resizeGrip = c; }} />
     </div>;
   }
 
