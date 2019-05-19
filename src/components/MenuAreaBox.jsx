0b3a3a201f1b5c1347954bfe3826e2da91fef0dd
Ari Petäjäjärvi <fpvonly@gmail.com>
2019-05-19 18:50:49 +0300

    Fixed bug that was caused by similar events and func names

--- src/components/MenuAreaBox.jsx
+++ src/components/MenuAreaBox.jsx
@@ -11,9 +11,9 @@ class MenuAreaBox extends React.Component {
     this.resizeGrip = null;
     this.panel = null;
     this.titleInput = null;
-    this.startX = null;
-    this.startY = null;
-    this.startWidth = -1;
+
+    this.resizeSstartX = null;
+    this.resizeStartWidth = -1;
     this.dragStartX = -1;
 
     this.state = {
@@ -36,35 +36,37 @@ class MenuAreaBox extends React.Component {
   };
 
   componentDidMount() {
-    this.resizeGrip.addEventListener('mousedown', this.handleMouseDown, false);
+    this.resizeGrip.addEventListener('mousedown', this.handleResizeColumnStart, false);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('mousemove', this.handleMouseMove, false);
+    window.removeEventListener('mousemove', this.handleResizeColumnMove, false);
   }
 
-  handleMouseDown = (e) => {
-    this.startX = e.pageX;
-    this.startY = e.pageY;
-    this.startWidth = this.panel.getBoundingClientRect().width;
-    this.startHeight = this.panel.getBoundingClientRect().height;
-    window.addEventListener('mousemove', this.handleMouseMove, false);
-    window.addEventListener('mouseup', this.handleMouseUp, false);
+  // resize column
+
+  handleResizeColumnStart = (e) => {
+    e.stopPropagation();
+    this.resizeSstartX = e.pageX;
+    this.resizeStartWidth = this.panel.getBoundingClientRect().width;
+    window.addEventListener('mousemove', this.handleResizeColumnMove, false);
+    window.addEventListener('mouseup', this.handleResizeColumnEnd, false);
+
   }
 
-  handleMouseMove = (e) => {
+  handleResizeColumnMove = (e) => {
     let curWidth = this.panel.getBoundingClientRect().width;
-    let diffX = (curWidth >= 200) ? this.startX - e.pageX : curWidth;
-    let newWidth = this.startWidth - diffX;
+    let diffX = (curWidth >= 200) ? this.resizeSstartX - e.pageX : curWidth;
+    let newWidth = this.resizeStartWidth - diffX;
 
     if (curWidth >= 200) {
       this.panel.style.width = newWidth + 'px';
     }
   }
 
-  handleMouseUp = () => {
+  handleResizeColumnEnd = () => {
     let endWidth = this.panel.getBoundingClientRect().width;
-    let colRelation = endWidth - this.startWidth;
+    let colRelation = endWidth - this.resizeStartWidth;
     let containerWidth = this.props.getContentPanelObj().getBoundingClientRect().width;
     let singleColWidth = containerWidth / 3;
     let endCols = Math.ceil(endWidth / singleColWidth);
@@ -73,26 +75,28 @@ class MenuAreaBox extends React.Component {
     this.setState({
       size: (endCols > 3 ? 3 : endCols)
     });
-    window.removeEventListener('mousemove', this.handleMouseMove, false);
+    window.removeEventListener('mousemove', this.handleResizeColumnMove, false);
   }
 
-  onMouseDown = (e) => {
+  // Drag column
+
+  handleDragColumnStart = (e) => {
+    e.stopPropagation();
     this.dragStartX = e.pageX;
-    window.addEventListener('mousemove', this.handleMouseMove, false);
-    window.addEventListener('mouseup', this.handleMouseUp, false);
+    window.addEventListener('mousemove', this.handleDragColumnMove, false);
+    window.addEventListener('mouseup', this.handleDragColumnEnd, false);
   }
 
-  handleMouseMove = (e) => {
+  handleDragColumnMove = (e) => {
     let dist = e.pageX - this.dragStartX;
     this.panel.style.left = dist + 'px';
   }
 
-  handleMouseUp = () => {
+  handleDragColumnEnd = () => {
     this.panel.style.left = 'auto';
     this.panel.style.top = 'auto';
-
-    window.removeEventListener('mousemove', this.handleMouseMove, false);
-    window.removeEventListener('mouseup', this.handleMouseUp, false);
+    window.removeEventListener('mousemove', this.handleDragColumnMove, false);
+    window.removeEventListener('mouseup', this.handleDragColumnEnd, false);
   }
 
 
@@ -103,7 +107,7 @@ class MenuAreaBox extends React.Component {
     };
 
     return <div className='box' ref={(c) => { this.panel = c; }} style={styles}>
-      <input className='title_input' ref={(c) => { this.titleInput = c; }} type='text' defaultValue='Title' onMouseDown={this.onMouseDown} />
+      <input className='title_input' ref={(c) => { this.titleInput = c; }} type='text' defaultValue='Title' onMouseDown={this.handleDragColumnStart} />
       {this.props.text}
       <div className='box_resize_grip' ref={(c) => { this.resizeGrip = c; }} />
     </div>;
